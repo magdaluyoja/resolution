750c6ea6776ec561590261cbc6a52feeea3b8d2e
magdaluyoja <magdaluyoja@gmail.com>
2017-08-22 16:14:41 +0800

    Helpers

--- client/main.js
+++ client/main.js
@@ -2,3 +2,16 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+if(Meteor.isClient){
+	Template.body.helpers({
+		resolutions:[
+			{title:"My resolution no. 1."}
+		]
+	});
+}
+
+if(Meteor.isServer){
+	Meteor.startup(function(){
+		//code to run at server startup
+	});
+}
